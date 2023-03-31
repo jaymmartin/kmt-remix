dccff7e0db888a5eedf2e57428cdbbced2e7d779
Jason Martin <jay.m.martin@gmail.com>
2023-03-31 10:03:33 -0400

    Add favicon links to head

--- app/root.tsx
+++ app/root.tsx
@@ -27,6 +27,12 @@ export function links() {
     { rel: "preconnect", href: "https://fonts.googleapis.com"},
     { rel: "preconnect", href: "https://fonts.gstatic.com", crossOrigin:"anonymous"},
     { rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Manrope&display=swap"},
+
+    //favicons
+    { rel: "apple-touch-icon", sizes:"180x180", href: "/apple-touch-icon.png"},
+    { rel: "icon", type:"image/png", sizes:"32x32", href: "/favicon-32x32.png"},
+    { rel: "icon", type:"image/png", sizes:"16x16", href: "/favicon-16x16.png"},
+    { rel: "manifest", href: "/site.webmanifest"}
   ];
 }
 
