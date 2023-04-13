dc8f80d4d0b57fee2f91f70d8e914dd3454299d2
jaymmartin <jay.m.martin@gmail.com>
2023-04-13 14:50:19 -0400

    Update About Page (#1)
    * Remove normalize.css
    
    * Remove custom font sizes
    
    * Refine h1/h2 tags for better SEO
    
    * Update favicon
    
    * Add favicon links to head
    
    * Add dark mode
    
    * Fix phone number link to real phone number
    
    * Resize images to web sizes
    
    * Add image to rates route
    
    * Add rounded corners to services image
    
    * Create subtitle in navbar
    
    * Add attributions to images
    
    * Add headshot to about page

--- app/routes/about.tsx
+++ app/routes/about.tsx
@@ -1,6 +1,6 @@
 import type { MetaFunction } from "@remix-run/node";
 import UnorderedList from "~/components/UnorderedList";
-import Main from '../components/Main'
+import Main from '../components/Main';
 
 export const meta: MetaFunction = () => {
   return {
@@ -15,31 +15,37 @@ export const meta: MetaFunction = () => {
 export default function About() {
   return (
     <Main>
-      <h1>About</h1>
-      <div>
-        <div className="mb-3">Kate Martin, LPC</div>
-        <ul className="mb-3">
-          <li>Clinically licensed therapist in New Jersey</li>
-          <li>MA, Kean University</li>
-          <li>BA, Duke University</li>
-        </ul>
-        <div className="space-y-4">
-          <div>Kate works with clients interested in:</div>
-          <UnorderedList>
-            <li>Coping with anxiety, anger, and sadness</li>
-            <li>Managing stress at home, work, and school</li>
-            <li>Improving the quality of their lives and relationships with others</li>
-            <li>Developing effective communication and the ability to advocate for themselves</li>
-            <li>Parenting strategies, skills, and support</li>
-            <li>Navigating life transitions and changes</li>
-            <li>Exploring body image and relationships with food</li>
-            <li>Understanding themselves and their connection to others</li>
-            <li>LGBTQIA+ issues and experiences</li>
-            <li>Practicing self-compassion and building confidence</li>
-            <li>Learning practical skills for managing time and responsibilities</li>
-            <li>Finding ways to invite joy and peace into their lives</li>
-          </UnorderedList>
-          <div>Kate Martin Therapy is an inclusive practice welcoming clients of all backgrounds, races, ethnic identities, countries of origin, genders, sexual orientations, faiths, abilities, and bodies.</div>
+      <div className="flex flex-wrap justify-between space-y-3 md:space-y-0 mt-6">
+        <div className="basis-full md:basis-5/12">
+          <img className="object-cover h-72 md:h-fit w-full rounded-xl" src="images/kate.webp" alt="Kate Martin smiling"/>
+        </div>
+        <div className="basis-full md:basis-6/12">
+          
+      <h1 className="font-bold text-3xl mb-6">About</h1>
+          <div className="mb-3">Kate Martin, LPC</div>
+          <ul className="mb-3">
+            <li>Clinically licensed therapist in New Jersey</li>
+            <li>MA, Kean University</li>
+            <li>BA, Duke University</li>
+          </ul>
+          <div className="space-y-4">
+            <div>Kate works with clients interested in:</div>
+            <UnorderedList>
+              <li>Coping with anxiety, anger, and sadness</li>
+              <li>Managing stress at home, work, and school</li>
+              <li>Improving the quality of their lives and relationships with others</li>
+              <li>Developing effective communication and the ability to advocate for themselves</li>
+              <li>Parenting strategies, skills, and support</li>
+              <li>Navigating life transitions and changes</li>
+              <li>Exploring body image and relationships with food</li>
+              <li>Understanding themselves and their connection to others</li>
+              <li>LGBTQIA+ issues and experiences</li>
+              <li>Practicing self-compassion and building confidence</li>
+              <li>Learning practical skills for managing time and responsibilities</li>
+              <li>Finding ways to invite joy and peace into their lives</li>
+            </UnorderedList>
+            <div>Kate Martin Therapy is an inclusive practice welcoming clients of all backgrounds, races, ethnic identities, countries of origin, genders, sexual orientations, faiths, abilities, and bodies.</div>
+          </div>
         </div>
       </div>
     </Main>
