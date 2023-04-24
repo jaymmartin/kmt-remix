0e179a5a3fc8ddc09fcf6629f936c7d076790eae
Jason Martin <jay.m.martin@gmail.com>
2023-04-24 17:17:21 -0400

    Add facebook link to footer

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,16 +1,26 @@
-import { Mail, Phone } from 'react-feather';
+import { Mail, Phone, Facebook, Instagram } from 'react-feather';
 
 export default function Footer() {
   return (
     <footer className="my-3 md:my-6">
-      <a className="flex space-x-1 md:space-x-3 mb-3" href="mailto:info@katemartintherapy.com">
+      <div className="flex space-x-3">
+        <a className="flex space-x-1 md:space-x-3 mb-3" href="mailto:info@katemartintherapy.com">
         <Mail />
         <span className="ml-3">info@katemartintherapy.com</span>
       </a>
-      <a className="flex space-x-1 md:space-x-3" href="tel:6463971025">
+      <a className="flex space-x-1 md:space-x-3 mb-3" href="tel:6463971025">
         <Phone />
         <span>646-397-1025</span>
       </a>
+      </div>
+      <div className="flex space-x-3">
+      <a className="flex space-x-1 md:space-x-3" href="https://www.facebook.com/profile.php?id=100091974518354" target="_blank">
+        <Facebook />
+      </a>
+      <a className="flex space-x-1 md:space-x-3" href="https://www.facebook.com/profile.php?id=100091974518354" target="_blank">
+        <Instagram />
+      </a>
+      </div>
     </footer>
 
   );
