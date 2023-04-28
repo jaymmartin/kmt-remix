6e3b818a6c13cabd5148ee7b5056ec21373a6f8f
Jason Martin <jay.m.martin@gmail.com>
2023-04-28 09:14:47 -0400

    Add accessibility labels

--- app/components/Image.tsx
+++ app/components/Image.tsx
@@ -33,6 +33,21 @@ const defaultProps = {
   licenseLink: "https://creativecommons.org/licenses/by/3.0/us/legalcode"
 }
 
+const cyrb53 = (str, seed = 0) => {
+  let h1 = 0xdeadbeef ^ seed, h2 = 0x41c6ce57 ^ seed;
+  for(let i = 0, ch; i < str.length; i++) {
+      ch = str.charCodeAt(i);
+      h1 = Math.imul(h1 ^ ch, 2654435761);
+      h2 = Math.imul(h2 ^ ch, 1597334677);
+  }
+  h1  = Math.imul(h1 ^ (h1 >>> 16), 2246822507);
+  h1 ^= Math.imul(h2 ^ (h2 >>> 13), 3266489909);
+  h2  = Math.imul(h2 ^ (h2 >>> 16), 2246822507);
+  h2 ^= Math.imul(h1 ^ (h1 >>> 13), 3266489909);
+
+  return 4294967296 * (2097151 & h2) + (h1 >>> 0);
+};
+
 export default function Image(props: ImageProps) {
   props = { ...defaultProps, ...props };
 
@@ -45,9 +60,10 @@ export default function Image(props: ImageProps) {
   if (props.author)
     author = "by " + props.author + " "
 
+  const id = "img-id-"+cyrb53(props.title)
   return <div className="">
-    <img className={`object-cover rounded-xl max-w-screen-lg w-full m-auto ${props.className}`} src={props.src} alt={props.alt} />
-    <div className="text-gray-400 text-[7px] md:text-[9px] w-full text-end">
+    <img aria-labelledby={id} className={`object-cover rounded-xl max-w-screen-lg w-full m-auto ${props.className}`} src={props.src} alt={props.alt} />
+    <div id={id} className="text-gray-400 text-[7px] md:text-[9px] w-full text-end">
       {title}
       {author}
       {licenseLink}
